ebbe6dfef1008ba534c8c751218bf9f06c7ec397
sudonnoh <sudonnoh@gmail.com>
2022-12-26 10:22:23 +0900

    2022.12.26

--- Msg-Notification/Msg-Notification/AppDelegate.swift
+++ Msg-Notification/Msg-Notification/AppDelegate.swift
@@ -9,14 +9,20 @@ import UIKit
 import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         if #available(iOS 10.0, *) {
+            // 사용자의 알림 사용여부 세팅
             let notiCenter = UNUserNotificationCenter.current()
             notiCenter.requestAuthorization(options: [.alert, .badge, .sound]) { (didAllow, e) in }
+            // 알림 센터와 관련해서 이벤트가 발생하면 Delegate로 전달받도록 하는 역할
+            // 즉, 이벤트 감지 역할을 한다.
+            notiCenter.delegate = self
+            // 앱 실행 도중에 알림 메시지가 도착하면 userNotificationCenter(_:willPresent:withCompletionHandler:) 메소드가 자동으로 호출
+            // 사용자가 알림 메시지를 실제로 클릭하면 userNotificationCenter(_:didReceive:withCompletionHandler:) 메소드가 자동으로 호출
         } else {
             
         }
@@ -36,5 +42,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    // 앱이 실행 도중에 알림 메시지가 도착한 경우에 아래 내용이 출력된다.
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        if notification.request.identifier == "wakeup" {
+            let userInfo = notification.request.content.userInfo
+            print(userInfo["name"]!)
+        }
+
+        completionHandler([.badge, .list, .sound])
+    }
+    
+    // 이 메소드는 실제로 사용자가 알림 메시지를 클릭했을 경우에 실행된다.
+    // 알림 메세지의 정보는 response 매개변수에 담긴다.
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.notification.request.identifier == "wakeup" {
+            let userInfo = response.notification.request.content.userInfo
+            print(userInfo["name"]!)
+        }
+        
+        completionHandler()
+    }
+    
 }
 
--- Msg-Notification/Msg-Notification/ViewController.swift
+++ Msg-Notification/Msg-Notification/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,61 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var msg: UITextField!
+    @IBOutlet var datepicker: UIDatePicker!
+    
+    @IBAction func save(_ sender: Any) {
+        // 버전별로 로컬 알림을 구현 처리
+        if #available(iOS 10, *) {
+            // UserNotification 프레임워크를 사용한 로컬 알림
+            // 알림 동의 여부를 확인
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                if settings.authorizationStatus == UNAuthorizationStatus.authorized {
+                    // 알림 설정이 들어갈 곳
+                    DispatchQueue.main.async {
+                        // 알림 콘텐츠 정의
+                        let nContent = UNMutableNotificationContent()
+                        nContent.body = (self.msg.text)!
+                        nContent.title = "미리 알림"
+                        nContent.sound = UNNotificationSound.default
+                        
+                        // 발송 시각을 '지금으로 부터 *초 형식'으로 변환
+                        let time = self.datepicker.date.timeIntervalSinceNow
+                        // 발송 조건 정의
+                        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: time, repeats: false)
+                        // 발송 요청 객체 정의
+                        let request = UNNotificationRequest(identifier: "alarm", content: nContent, trigger: trigger)
+                        // 노티피케이션 센터에 로컬 알림을 등록하는 add(_:) 메소드는 두 번째 인자값으로 완료 시 호출 될 클로저를
+                        // 입력할 수 있다. 이를 이용해 메소드 뒤에 트레일링 클로저 형식으로 메시지 창을 호출하면 된다.
+                        UNUserNotificationCenter.current().add(request) { (_) in
+                            DispatchQueue.main.async {
+                                // 세계 표준시를 기준으로 보여주는데, 이를 우리나라 기준으로 변경하기 위해 9시간을 추가해야 한다.
+                                // addingTimeInterval 의 입력값의 단위가 초이기 때문에 9시간을 초 단위로 변경해서 입력한다.
+                                let date = self.datepicker.date.addingTimeInterval(9*60*60)
+                                let message = "알람이 등록되었습니다. 등록된 알람은 \(date)에 발송됩니다."
+                                
+                                let alert = UIAlertController(title: "알람등록", message: message, preferredStyle: .alert)
+                                
+                                let ok = UIAlertAction(title: "확인", style: .default)
+                                
+                                alert.addAction(ok)
+                                
+                                self.present(alert, animated: false)
+                            }
+                        }
+                    }
+                } else {
+                    let alert = UIAlertController(title: "알림등록", message: "알림이 허용되지 않았습니다.", preferredStyle: .alert)
+                    let ok = UIAlertAction(title:"확인", style: .default)
+                    alert.addAction(ok)
+                    
+                    self.present(alert, animated: false)
+                    return
+                }
+            }
+        } else {
+            // LocalNotification 객체를 사용한 로컬 알림
+        }
+    }
 }
 
